5d372f1043c8aadf510b9e0c920bc84f4f52485d
kmjnnhyk <rlawlsgur620@gmail.com>
2021-11-03 15:01:45 +0900

    Targeting elements and Targeting descendant elements study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.scss';
 import { gsap } from 'gsap';
+import React, { useEffect, useRef } from 'react';
 
 function App() {
+  const expText = useRef();
+  const container = useRef();
+  const underContainer = gsap.utils.selector(container);
+
+  useEffect(() => {
+    gsap.to(underContainer('.explanation'), {
+      left: 0,
+      stagger: 1.0,
+    });
+    gsap.to(expText.current, { rotation: '+=360' });
+  });
+
   return (
-    <div className='App'>
+    <div className='App' ref={container}>
       <div className='container'>
         <div className='explanation'>
-          <h1 className='text'>
+          <h1 className='text' ref={expText}>
             가운데 테이블에 놓여진 웹캠을 바라보고 아래의 문장 또는
             원하는 그 어떤 단어든지 말씀해보세요
           </h1>
@@ -14,9 +27,8 @@ function App() {
             ex. 한글은 소리를 담기에 가장 적합한 글자이다.
           </p>
         </div>
-      </div>
-      <div className='container'>
-        <div className='example2'>
+
+        <div className='explanation'>
           <h1>sound</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -25,9 +37,8 @@ function App() {
             quae ducimus enim architecto voluptates ipsum.
           </p>
         </div>
-      </div>
-      <div className='container'>
-        <div className='example3'>
+
+        <div className='explanation'>
           <h1>shape</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
